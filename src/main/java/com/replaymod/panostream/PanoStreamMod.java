1dd6ddcae229bb04432e76e1a4c7801796492977
CrushedPixel <crushedpixelmaps@gmail.com>
2016-01-26 22:37:20 +0100

    Initial commit

---
+++ src/main/java/com/replaymod/panostream/PanoStreamMod.java
@@ -0,0 +1,10 @@
+package com.replaymod.panostream;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = PanoStreamMod.MODID, useMetadata = true)
+public class PanoStreamMod {
+
+    public static final String MODID = "panostream";
+
+}
