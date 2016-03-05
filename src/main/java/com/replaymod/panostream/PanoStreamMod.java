aa56dbd4d6cb9d25e6783f3009ea019f4fa2d878
CrushedPixel <crushedpixelmaps@gmail.com>
2016-03-05 18:41:01 +0100

    Created PanoStreamSettings based on SimpleConfig project

--- src/main/java/com/replaymod/panostream/PanoStreamMod.java
+++ src/main/java/com/replaymod/panostream/PanoStreamMod.java
@@ -1,23 +1,20 @@
 package com.replaymod.panostream;
 
 import com.replaymod.panostream.capture.PanoramicFrameCapturer;
+import com.replaymod.panostream.settings.PanoStreamSettings;
 import com.replaymod.panostream.stream.VideoStreamer;
-import com.replaymod.panostream.utils.FrameSizeUtil;
 import lombok.Getter;
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.lwjgl.util.Dimension;
 
 @Mod(modid = PanoStreamMod.MODID, useMetadata = true)
 public class PanoStreamMod {
 
     public static final String MODID = "panostream";
-
-    public static final int DEFAULT_FPS = 30;
-    //public static final int DEFAULT_FRAMESIZE = 1080; //for 4k
-    public static final int DEFAULT_FRAMESIZE = 540; //for 1080p
-    public static final String DEFAULT_DESTINATION = "rtmp://127.0.0.1/panostream/minecraft";
-    //public static final String DEFAULT_DESTINATION = "equi.mp4";
 
     @Mod.Instance(value = MODID)
     public static PanoStreamMod instance;
@@ -30,10 +27,23 @@ public class PanoStreamMod {
     @Getter
     private VideoStreamer videoStreamer;
 
+    @Getter
+    private PanoStreamSettings panoStreamSettings;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        panoStreamSettings = new PanoStreamSettings(new Configuration(event.getSuggestedConfigurationFile()));
+        panoStreamSettings.init();
+    }
+
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-        videoStreamer = new VideoStreamer(FrameSizeUtil.composedFrameSize(DEFAULT_FRAMESIZE), DEFAULT_FPS, DEFAULT_DESTINATION);
-        panoramicFrameCapturer = new PanoramicFrameCapturer(DEFAULT_FRAMESIZE, videoStreamer).register();
+        videoStreamer = new VideoStreamer(new Dimension(panoStreamSettings.videoWidth.getIntValue(),
+                panoStreamSettings.videoHeight.getIntValue()), panoStreamSettings.fps.getIntValue(),
+                panoStreamSettings.rtmpServer.getStringValue());
+
+        panoramicFrameCapturer = new PanoramicFrameCapturer(panoStreamSettings.videoHeight.getIntValue() / 2,
+                videoStreamer).register();
     }
 
 }
---
+++ src/main/java/com/replaymod/panostream/settings/PanoStreamSettings.java
@@ -0,0 +1,27 @@
+package com.replaymod.panostream.settings;
+
+import eu.crushedpixel.minecraft.simpleconfig.ConfigSettings;
+import eu.crushedpixel.minecraft.simpleconfig.IntegerSetting;
+import eu.crushedpixel.minecraft.simpleconfig.StringSetting;
+import net.minecraftforge.common.config.Configuration;
+
+public class PanoStreamSettings extends ConfigSettings {
+    
+    public IntegerSetting fps = new IntegerSetting(30);
+
+    public IntegerSetting videoWidth = new IntegerSetting(2160);
+
+    public IntegerSetting videoHeight = new IntegerSetting(1080);
+
+    public StringSetting ffmpegCommand = new StringSetting("ffmpeg");
+
+    public StringSetting ffmpegArgs = new StringSetting("-re -f rawvideo -pix_fmt rgb24 -s %WIDTH%x%HEIGHT% -r %FPS% -i - " +
+            "-f flv -qmin 2 -qmax 25 -rtmp_buffer 100 -rtmp_live live %DESTINATION%");
+
+    public StringSetting rtmpServer = new StringSetting("rtmp://localhost/panostream/live");
+
+    public PanoStreamSettings(Configuration config) {
+        super(config);
+    }
+
+}
