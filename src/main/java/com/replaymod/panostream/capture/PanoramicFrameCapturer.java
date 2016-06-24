2e328acb895e9799642c4fa26f49f02b6afcc904
CrushedPixel <crushedpixelmaps@gmail.com>
2016-06-24 12:58:09 +0200

    Respect actual mouse position when rendering Gui Screens for the 360° view

--- src/main/java/com/replaymod/panostream/capture/PanoramicFrameCapturer.java
+++ src/main/java/com/replaymod/panostream/capture/PanoramicFrameCapturer.java
@@ -7,12 +7,14 @@ import com.replaymod.panostream.utils.ScaledResolutionUtil;
 import lombok.Getter;
 import lombok.Setter;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.shader.Framebuffer;
 import net.minecraftforge.client.ForgeHooksClient;
 import net.minecraftforge.client.event.RenderHandEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.GL11;
 
 
@@ -51,7 +53,6 @@ public class PanoramicFrameCapturer extends Registerable<PanoramicFrameCapturer>
     @SubscribeEvent
     public void capturePanoramicFrame(TickEvent.RenderTickEvent event) {
         if(!active || panoramicFrame == null) return;
-        //if(mc.currentScreen instanceof GuiMainMenu) return; //TODO: handle GuiMainMenu
         if(event.phase != TickEvent.Phase.END) return;
 
         long curTime = System.currentTimeMillis();
@@ -73,13 +74,19 @@ public class PanoramicFrameCapturer extends Registerable<PanoramicFrameCapturer>
 
         mc.displayWidth = mc.displayHeight = panoramicFrame.getFrameSize();
 
+        //calculate the mouse location on the distorted gui
+        ScaledResolution actualResolution = new ScaledResolution(mc, widthBefore, heightBefore);
+
+        int mouseX = Mouse.getX() * actualResolution.getScaledWidth() / widthBefore;
+        int mouseY = actualResolution.getScaledHeight() - Mouse.getY() * actualResolution.getScaledHeight() / heightBefore - 1;
+
         for(int i=0; i<6; i++) {
             CaptureState.setOrientation(Orientation.values()[i]);
 
             panoramicFrame.bindFramebuffer(i);
 
             renderWorld();
-            renderOverlays();
+            renderOverlays(mouseX, mouseY);
 
             panoramicFrame.unbindFramebuffer();
         }
@@ -102,7 +109,7 @@ public class PanoramicFrameCapturer extends Registerable<PanoramicFrameCapturer>
         mc.entityRenderer.renderWorldPass(2, mc.timer.elapsedPartialTicks, 0);
     }
 
-    private void renderOverlays() {
+    private void renderOverlays(int mouseX, int mouseY) {
         if(this.mc.gameSettings.hideGUI && this.mc.currentScreen == null) return;
 
         //if a GuiScreen is opened, render the default overlay on the other sides
@@ -124,7 +131,7 @@ public class PanoramicFrameCapturer extends Registerable<PanoramicFrameCapturer>
                 mc.entityRenderer.setupOverlayRendering(); //re-setup overlay rendering with distortion enabled
                 CaptureState.setDistortGUI(false);
                 GlStateManager.clear(GL11.GL_DEPTH_BUFFER_BIT);
-                ForgeHooksClient.drawScreen(mc.currentScreen, 0, 0, 0);
+                ForgeHooksClient.drawScreen(mc.currentScreen, mouseX, mouseY, mc.timer.renderPartialTicks);
             }
 
             mc.framebufferMc = before;
@@ -136,27 +143,6 @@ public class PanoramicFrameCapturer extends Registerable<PanoramicFrameCapturer>
         //don't render the hand in every frame
         if(CaptureState.isCapturing() && CaptureState.getOrientation() != Orientation.FRONT) event.setCanceled(true);
     }
-
-    /* Maybe we'll want to capture the GUI only on a separate Framebuffer later
-    @SubscribeEvent
-    public void onRenderGui(GuiScreenEvent.DrawScreenEvent event) {
-        //render the Gui on the PanoramicFrameCapturer's Gui Framebuffer
-        PanoramicFrameCapturer capturer = PanoStreamMod.instance.getPanoramicFrameCapturer();
-
-        if(capturer.isActive() && capturer.getOrientation() == null) {
-            Framebuffer framebuffer = capturer.getGuiFramebuffer();
-            if(framebuffer.framebufferWidth != mc.displayWidth || framebuffer.framebufferHeight != mc.displayHeight) {
-                framebuffer.createBindFramebuffer(mc.displayWidth, mc.displayHeight);
-            } else {
-                framebuffer.bindFramebuffer(true);
-            }
-
-            event.gui.drawScreen(event.mouseX, event.mouseY, event.renderPartialTicks);
-
-            mc.getFramebuffer().bindFramebuffer(true);
-        }
-    }
-    */
 
     public enum Orientation {
         FRONT, BACK, LEFT, RIGHT, BOTTOM, TOP;
